1aa63b98ab3942e38ff538f96ff8b83994321093
Kevin Darcy <neoglitch822@yahoo.com>
2019-11-29 18:09:53 -0500

    player movement

--- player_movement/game.js
+++ player_movement/game.js
@@ -20,13 +20,69 @@ var config = {
             player: null,
             reticle: null,
             moveKeys: null,
-            bullets: null,
+            playerBullets: null,
             time: 0,
         }
     }
 };
 
 var game = new Phaser.Game(config);
+
+/**/var Bullet = new Phaser.Class({
+
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+
+    // Bullet Constructor
+    function Bullet (scene)
+    {
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+        this.speed = 1;
+        this.born = 0;
+        this.direction = 0;
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        this.setSize(12, 12, true);
+    },
+
+    // Fires a bullet from the player to the reticle
+    fire: function (shooter, target)
+    {
+        this.setPosition(shooter.x, shooter.y); // Initial position
+        this.direction = Math.atan( (target.x-this.x) / (target.y-this.y));
+
+        // Calculate X and y velocity of bullet to moves it from shooter to target
+        if (target.y >= this.y)
+        {
+            this.xSpeed = this.speed*Math.sin(this.direction);
+            this.ySpeed = this.speed*Math.cos(this.direction);
+        }
+        else
+        {
+            this.xSpeed = -this.speed*Math.sin(this.direction);
+            this.ySpeed = -this.speed*Math.cos(this.direction);
+        }
+
+        this.rotation = shooter.rotation; // angle bullet with shooters rotation
+        this.born = 0; // Time since new bullet spawned
+    },
+
+    // Updates the position of the bullet each cycle
+    update: function (time, delta)
+    {
+        this.x += this.xSpeed * delta;
+        this.y += this.ySpeed * delta;
+        this.born += delta;
+        if (this.born > 1800)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+    }
+
+});
+/**/
 
 function preload() {
     // Load in images and sprites
@@ -36,11 +92,14 @@ function preload() {
     });
     this.load.image('target', 'crosshair.png');
     this.load.image('background', 'floor.png');
+    this.load.image('bullet', 'bullet6.png');
 }
 
 function create() {
     // Create world bounds
     this.physics.world.setBounds(0, 0, 1600, 1200);
+
+    playerBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
 
     // Add background, player, and reticle sprites
     var background = this.add.image(800, 600, 'background');
@@ -53,7 +112,9 @@ function create() {
     reticle.setOrigin(0.5, 0.5).setDisplaySize(25, 25).setCollideWorldBounds(true);
 
     // Set camera zoom
-    this.cameras.main.zoom = 0.5;
+    // this.cameras.main.zoom = 0.5;
+    this.cameras.main.zoom = 0.5; /* NEW*/
+    this.cameras.main.startFollow(player);
 
     // Creates object for input with WASD kets
     moveKeys = this.input.keyboard.addKeys({
@@ -94,6 +155,19 @@ function create() {
         if (moveKeys['left'].isUp)
             player.setAccelerationX(0);
     });
+
+    // lanuch bullet
+    this.input.on('pointerdown', function (pointer, time, lastFired) {
+        if (player.active === false)
+            return;
+
+        // Get bullet
+        var bullet = playerBullets.get().setActive(true).setVisible(true);
+        if (bullet) {
+            bullet.fire(player, reticle);
+            this.physics.add.collider(enemy, bullet, enemyHitCallback);
+        }
+    }, this);
 
     // Locks pointer on mousedown
     game.canvas.addEventListener('mousedown', function () {
